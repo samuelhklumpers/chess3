b5ad06b845582bad387973927ff68ab845f5587f
Samuel Klumpers <samuelhklumpers@gmail.com>
2023-01-07 10:25:19 +0100

    make servers more systemd compatible

--- chess/server/menuserver.py
+++ chess/server/menuserver.py
@@ -3,9 +3,15 @@ import socketserver
 
 from functools import partial
 
-PORT = 80
+import os
+import sys
+os.chdir(sys.path[0])
+
+
+PORT = 8080
 handler = partial(http.server.SimpleHTTPRequestHandler, directory="./menu")
 
 with socketserver.TCPServer(("", PORT), handler) as http_server:
+    print(os.getcwd())
     print(http_server.server_address)
     http_server.serve_forever()
--- serverloop.py
+++ serverloop.py
@@ -4,14 +4,16 @@ import json
 
 from chess.server.gameserver import thread_loop
 
+import os
+import sys
+os.chdir(sys.path[0])
+
 with open("server_config.json", encoding="utf-8") as f:
     config = json.load(f)
 
 port = config["port"]
 
 if __name__ == "__main__":
-    import os
-
     print(os.getpid())
     thread_loop(port)
     print("exited gracefully")
